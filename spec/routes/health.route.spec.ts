f0d63e5baea12b3d46694f98896f9a60e571e13f
JT Wheeler <jwheeler@JTs-MacBook-Pro.local>
2018-02-14 12:45:02 -0500

    Renamed request to supertest

--- spec/routes/health.route.spec.ts
+++ spec/routes/health.route.spec.ts
@@ -1,10 +1,10 @@
 /// <reference path="../../node_modules/@types/jasmine/index.d.ts" />
-import * as request from 'supertest';
+import * as supertest from 'supertest';
 import * as app from '../../src/app';
 
 describe('GET /api/health', () => {
     it('should return 200 OK', () => {
-        request(app).get('/api/health')
+        supertest(app).get('/api/health')
             .expect(200)
             .end((error: any, response: any) => {
                 if (error)
--- spec/routes/hello.route.spec.ts
+++ spec/routes/hello.route.spec.ts
@@ -1,10 +1,10 @@
 /// <reference path="../../node_modules/@types/jasmine/index.d.ts" />
-import * as request from 'supertest';
+import * as supertest from 'supertest';
 import * as app from '../../src/app';
 
 describe('GET /api/hello', () => {
     it('should return 200 OK', () => {
-        request(app).get('/api/hello/')
+        supertest(app).get('/api/hello/')
             .expect(200)
             .end((error: any, response: any) => {
                 if (error) {
