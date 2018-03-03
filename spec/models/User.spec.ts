5966e19bb6767c8784b48ae3a4aed6650df1ffae
JT Wheeler <jwheeler@JTs-MacBook-Pro.local>
2018-03-03 15:12:26 -0500

    Changed spec to use Jasmine done function instead of returning promises

--- spec/models/User.spec.ts
+++ spec/models/User.spec.ts
@@ -7,51 +7,51 @@ import * as bluebird from 'bluebird';
 const chance = new Chance();
 
 describe('User model', function () {
-    let email: string;
-    let password: string;
-    let newUser: any;
-
     beforeEach(function () {
-        email = chance.email();
-        password = chance.string();
+        this.email = chance.email();
+        this.password = chance.string();
 
-        newUser = new User({
-            email: email,
-            password: password
+        this.newUser = new User({
+            email: this.email,
+            password: this.password
         });
     });
 
-    beforeAll(function () {
+    beforeAll(function (done) {
         (<any>mongoose).Promise = bluebird;
-        return mongoose.connect(config.mongoUri);
+        mongoose.connect(config.mongoUri)
+            .then(done, done.fail);
     });
 
-    afterAll(function () {
-        return mongoose.disconnect();
+    afterAll(function (done) {
+        mongoose.disconnect()
+            .then(done, done.fail);
     });
 
-    it('save() should add new user to database', function () {
-        return newUser.save()
+    it('save() should add new user to database', function (done) {
+        this.newUser.save()
             .then((user: UserModel) => {
-                return User.findOne({ email: email }, (error: any, user: any) => {
+                User.findOne({ email: this.email }, (error: any, user: any) => {
                     if (error) {
                         throw error;
                     }
 
                     expect(user).toBeTruthy();
-                    expect(user.email).toBe(email);
+                    expect(user.email).toBe(this.email);
                 });
-            });
+            })
+            .then(done, done.fail);
     });
 
-    it('comparePassword() should return false when wrong password is entered', function () {
-        return newUser.save()
+    it('comparePassword() should return false when wrong password is entered and true if correct password is enter', function (done) {
+        this.newUser.save()
             .then((user: UserModel) => {
-                return User.findOne({ email: email }, (error: any, user: any) => {
+                User.findOne({ email: this.email }, (error: any, user: any) => {
                     if (error) {
                         console.log(error);
                         throw error;
                     }
+
                     user.comparePassword('wrongPassword', (error: Error, isMatch: boolean) => {
                         if (error) {
                             throw error;
@@ -59,7 +59,16 @@ describe('User model', function () {
 
                         expect(isMatch).toBe(false);
                     });
+
+                    user.comparePassword(this.password, (error: Error, isMatch: boolean) => {
+                        if (error) {
+                            throw error;
+                        }
+
+                        expect(isMatch).toBe(true);
+                    });
                 });
-            });
+            })
+            .then(done, done.fail);
     });
 });
