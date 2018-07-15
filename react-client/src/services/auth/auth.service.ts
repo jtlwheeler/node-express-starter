ddc485d2525042f12a8d9ef1ce8788f74a9c4e01
JT Wheeler <jtlwheeler@gmail.com>
2018-07-15 11:50:59 -0400

    Changed auth service to create request config locally instead of using class member.

--- react-client/src/services/auth/auth.service.ts
+++ react-client/src/services/auth/auth.service.ts
@@ -4,31 +4,33 @@ import * as HttpStatus from 'http-status';
 
 export class AuthService {
 
-    private config: AxiosRequestConfig = {
-        baseURL: 'http://localhost:3001'
-    };
+    private baseURL = 'http://localhost:3001';
 
     public async login(email: string, password: string): Promise<Token> {
-        this.config = {};
-        this.config.url = '/api/auth/login';
-        this.config.method = 'post';
-        this.config.data = {
-            email: email,
-            password: password
+        const config: AxiosRequestConfig = {
+            baseURL: this.baseURL,
+            url: '/api/auth/login',
+            method: 'post',
+            data: {
+                email: email,
+                password: password
+            }
         };
 
-        const response = await axios.request(this.config);
+        const response = await axios.request(config);
         return response.data.token;
     }
 
     public async registerUser(registerUserRequest: any): Promise<boolean> {
-        this.config = {};
-        this.config.url = '/api/user/signUp/';
-        this.config.method = 'post';
-        this.config.data = registerUserRequest;
+        const config: AxiosRequestConfig = {
+            baseURL: this.baseURL,
+            url:  '/api/user/signUp/',
+            method: 'post',
+            data: registerUserRequest
+        };
 
         try {
-            const response = await axios.request(this.config);
+            const response = await axios.request(config);
             return response.status === HttpStatus.OK;
         } catch (error) {
             throw error;
@@ -36,13 +38,17 @@ export class AuthService {
     }
 
     public async checkToken(token: Token): Promise<boolean> {
-        this.config = {};
-        this.config.url = '/api/auth/checkToken/';
-        this.config.method = 'get';
-        this.config.params = {
-            token: token.token
+        const config: AxiosRequestConfig = {
+            baseURL: this.baseURL,
+            url:  '/api/auth/checkToken/',
+            method: 'get',
+            params: {
+                token: token.token
+
+            }
         };
-        const response = await axios.request(this.config);
+
+        const response = await axios.request(config);
         return response.data.isTokenValid;
     }
 }
