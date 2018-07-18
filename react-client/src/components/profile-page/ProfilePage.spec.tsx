b0d81c1f205283780ae9e01513f2303c2889c06c
JT Wheeler <jtlwheeler@gmail.com>
2018-07-18 09:17:30 -0400

    Retrieve secret and display message on profile page.

--- react-client/src/components/profile-page/ProfilePage.spec.tsx
+++ react-client/src/components/profile-page/ProfilePage.spec.tsx
@@ -1,13 +1,43 @@
 import { shallow, configure } from 'enzyme';
 import * as React from 'react';
 import * as Adapter from 'enzyme-adapter-react-16';
-import { ProfilePage } from './ProfilePage';
+import authService from '../../services/auth/auth.service';
+import * as sinon from 'sinon';
+import { waitUntil } from '../test-helpers/waitUntil.helper';
+import ProfilePage from './ProfilePage';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
 describe('<ProfilePage />', function () {
     it('should render profile page', function () {
-        const wrapper = shallow(<ProfilePage />);
+        const token = {
+            token: 'theAccessToken'
+        };
+        const wrapper = shallow(<ProfilePage token={token}/>);
         expect(wrapper.find('.profile-page').length).toBe(1);
+    });
+
+    fit('should call getSecret when page loads and render success on UI', async function () {
+        const authServiceStub = sinon.stub(authService, 'getSecret')
+            .returns(Promise.resolve(true));
+
+        const token = {
+            token: 'theAccessToken'
+        };
+
+        const wrapper = shallow(<ProfilePage token={token}/>);
+
+        await waitUntil(() => authServiceStub.called,
+            10,
+            'getSecret was never called');
+
+        sinon.assert.calledWith(authServiceStub, token);
+
+        await waitUntil(() => wrapper.update().find('.secret-message').length === 1,
+            100,
+            'secret message was never shown');
+        expect(wrapper.update().find('.secret-message').length).toBe(1);
+
+        authServiceStub.restore();
     });
 });
--- react-client/src/components/profile-page/ProfilePage.tsx
+++ react-client/src/components/profile-page/ProfilePage.tsx
@@ -1,7 +1,40 @@
 import * as React from 'react';
+import authService from '../../services/auth/auth.service';
+import { Token } from '../../../../../my-app/src/services/authService';
 
-export const ProfilePage = (props: any) => (
-    <div className="profile-page" >
-        <h1>Profile Page</h1>
-    </div>
-);
+export interface ProfilePageProps {
+    token: Token;
+}
+
+interface State {
+    accessedSecret: boolean;
+}
+
+export default class ProfilePage extends React.Component<ProfilePageProps, State> {
+
+    constructor(props: ProfilePageProps) {
+        super(props);
+
+        this.state = {
+            accessedSecret: false
+        };
+    }
+
+    async componentDidMount() {
+        const accessedSecret = await authService.getSecret(this.props.token);
+        console.log(accessedSecret);
+        this.setState({accessedSecret: accessedSecret});
+    }
+
+    render() {
+        console.log(this.state.accessedSecret);
+        return (
+            <div className="profile-page">
+                <h1>Profile Page</h1>
+                {this.state.accessedSecret &&
+                <h2 className="secret-message">You got the secret!</h2>
+                }
+            </div>
+        );
+    }
+}
