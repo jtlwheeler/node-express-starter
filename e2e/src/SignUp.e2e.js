8189d68b5c7bd9afe984c6f452794719819c86e8
JT Wheeler <jtlwheeler@gmail.com>
2018-08-01 20:37:04 -0400

    Sign up e2e test also logs in with the new user.

--- e2e/src/SignUp.e2e.js
+++ e2e/src/SignUp.e2e.js
@@ -2,7 +2,7 @@ const Chance = require('chance');
 const chance = new Chance();
 
 describe('Create Account', function () {
-    it('should create a new user and redirect to the login page', async function () {
+    it('should create a new user and redirect to the login page and then log new user in', async function () {
         browser.get('/register');
 
         const email = chance.email();
@@ -19,11 +19,27 @@ describe('Create Account', function () {
         const submitButton = element(by.css('button.submit-button'));
         submitButton.click();
 
-        let expectedUrl = `${browser.baseUrl}/login`;
+        const expectedUrl = `${browser.baseUrl}/login`;
         await browser.wait(async () =>
             (await browser.getCurrentUrl()) === expectedUrl, 10000);
 
         const currentUrl = await browser.getCurrentUrl();
         expect(currentUrl).toBe(expectedUrl);
+
+        const loginEmailInput = element(by.css('input.email'));
+        loginEmailInput.sendKeys(email);
+
+        const loginPasswordInput = element(by.css('input.password'));
+        loginPasswordInput.sendKeys(password);
+
+        const loginSubmitButton = element(by.css('button.submit-button'));
+        loginSubmitButton.click();
+
+        const profileUrl = `${browser.baseUrl}/profile`;
+        await browser.wait(async () =>
+            (await browser.getCurrentUrl()) === profileUrl, 10000);
+
+        const secretText = element(by.css('.secret-message'));
+        expect(secretText.getText()).toBe('You got the secret!')
     });
 });
